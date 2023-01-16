9d42743450234977d981340681c219326d30ca65
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-16 11:25:05 -0600

    Initial Commit

---
+++ Snacktackular/SnacktackularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktackularApp.swift
+//  Snacktackular
+//
+//  Created by Jeremy Taylor on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktackularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
