897101af306060447159a88c0a73256a93e28e00
Jeremy Taylor <jtaylor@jctaylor.org>
2023-01-16 12:52:02 -0600

    Sign Up and Login with Firebase

---
+++ Snacktackular/LoginView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  Snacktackular
+//
+//  Created by Jeremy Taylor on 1/16/23.
+//
+
+import SwiftUI
+import Firebase
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    
+    var body: some View {
+        NavigationStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Group {
+                TextField("E-mail", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.next)
+                SecureField("Password", text: $password)
+                    .textInputAutocapitalization(.never)
+                    .submitLabel(.done)
+            }
+            .textFieldStyle(.roundedBorder)
+            .overlay {
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(.gray.opacity(0.5), lineWidth: 2)
+            }
+            .padding(.horizontal)
+            
+            HStack {
+                Button {
+                    register()
+                } label: {
+                    Text("Sign-Up")
+                }
+                .padding(.trailing)
+                
+                Button {
+                    login()
+                } label: {
+                    Text("Login")
+                }
+                .padding(.leading)
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color("SnackColor"))
+            .font(.title2)
+            .padding(.top)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .alert(alertMessage, isPresented: $showingAlert) {
+            Button("OK", role: .cancel) {}
+        }
+    }
+    
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error { // login error occured
+                print("😡 SIGN-UP ERROR: \(error.localizedDescription)")
+                alertMessage = "SIGN-UP ERROR: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("😎 Registration success!")
+                //TODO: LoadListView
+            }
+        }
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error { // login error occured
+                print("😡 LOGIN ERROR: \(error.localizedDescription)")
+                alertMessage = "LOGIN ERROR: \(error.localizedDescription)"
+                showingAlert = true
+            } else {
+                print("🪵 Login successful!")
+                //TODO: LoadListView
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
