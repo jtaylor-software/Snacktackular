547e9694949d7339eaba14adb215eae412617871
Jeremy Taylor <jtaylor@jctaylor.org>
2023-02-04 13:28:32 -0600

    Added review for spots

---
+++ Snacktackular/Models/Review.swift
@@ -0,0 +1,23 @@
+//
+//  Review.swift
+//  Snacktackular
+//
+//  Created by Jeremy Taylor on 2/4/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Review: Identifiable, Codable {
+    @DocumentID var id: String?
+    var title = ""
+    var body = ""
+    var rating = 0
+    var reviewer = ""
+    var postedOn = Date()
+    
+    var dictionary: [String: Any] {
+        return ["title": title, "body": body, "rating": rating, "reviewer": Auth.auth().currentUser?.email ?? "", "postedOn": Timestamp(date: Date())]
+    }
+}
